07c7d427f823446f62501b2e3087f120103adb51
TheWeirdDee <divinenation1@gmail.com>
2026-05-06 06:08:32 +0000

    feat: enforce minimum prices for payment options (step 55)

--- contracts/MicroMindPayment.sol
+++ contracts/MicroMindPayment.sol
@@ -1,4 +1,4 @@
-// Solidity build revision 54
+// Solidity build revision 55
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -50,3 +50,4 @@ contract MicroMindPayment is Ownable, ReentrancyGuard {
         if (msg.value > 0) {
             require(msg.value >= price, "Insufficient CELO sent");
         } else {
+            require(
