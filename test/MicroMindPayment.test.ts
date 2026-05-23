c1f9f0f69743849b9b7bad5e6f246cb224d6a84f
TheWeirdDee <divinenation1@gmail.com>
2026-05-23 02:56:15 -0700

    test: add double payment failure test (step 125)

--- test/MicroMindPayment.test.ts
+++ test/MicroMindPayment.test.ts
@@ -1,6 +1,7 @@
-// Test revision 124
+// Test revision 125
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("MicroMindPayment", function () {
   it("Should set the correct payment token", async function () {
+    const [owner, token] = await ethers.getSigners();
