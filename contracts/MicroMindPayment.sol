0223f1a116675f27a1e49b5e91c12453ad3b24b0
TheWeirdDee <divinenation1@gmail.com>
2026-05-23 02:56:01 -0700

    docs: add MiniPay optimization requirements (step 84)

--- contracts/MicroMindPayment.sol
+++ contracts/MicroMindPayment.sol
@@ -1,4 +1,4 @@
-// Solidity build revision 83
+// Solidity build revision 84
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -77,3 +77,4 @@ contract MicroMindPayment is Ownable, ReentrancyGuard {
     function withdrawCelo(uint256 amount) external onlyOwner {
         (bool success, ) = owner().call{value: amount}("");
         require(success, "Withdraw failed");
+    }
