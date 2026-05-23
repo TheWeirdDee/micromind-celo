cdc264264b8db0a9a01b878336390ad1fa79608b
TheWeirdDee <divinenation1@gmail.com>
2026-05-23 02:55:38 -0700

    feat: verify prompt hash is unique before processing (step 7)

--- contracts/MicroMindPayment.sol
+++ contracts/MicroMindPayment.sol
@@ -1,6 +1,7 @@
-// Solidity build revision 6
+// Solidity build revision 7
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
