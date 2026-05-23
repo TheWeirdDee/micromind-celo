1a1620ec4af77a949bf11ec77aebc04f5c6ee6bf
TheWeirdDee <divinenation1@gmail.com>
2026-05-23 02:55:40 -0700

    feat: implement withdraw function for cUSD tokens (step 14)

--- contracts/MicroMindPayment.sol
+++ contracts/MicroMindPayment.sol
@@ -1,4 +1,4 @@
-// Solidity build revision 13
+// Solidity build revision 14
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -11,3 +11,4 @@ contract MicroMindPayment is Ownable, ReentrancyGuard {
 
     uint8 public constant TOOL_CHAT   = 0;
     uint8 public constant TOOL_RESUME = 1;
+    uint8 public constant TOOL_TWEET  = 2;
